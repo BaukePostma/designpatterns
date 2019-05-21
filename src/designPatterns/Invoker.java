f07c0932e96bb8ed3bc8c22ba2d21b78fffa218f
baukepostma <bauke.j.p@hotmail.com>
2019-05-21 10:55:40 +0200

    Forgot to add an invoker class to the command pattern. fixed that

---
+++ src/designPatterns/Invoker.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designPatterns;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Bauke
+ */
+public class Invoker {
+
+    ArrayList<ICommand> CommandHistory = new ArrayList<ICommand>();
+    int undocount = 0;
+
+    public void AddAction(ICommand c){
+        ClearHistory();
+        CommandHistory.add(c);
+        c.Execute();
+    }
+    public void DoAction() {
+        
+        if (undocount > 0) {
+            CommandHistory.get(CommandHistory.size() - undocount).Execute();
+            undocount--;
+        } else {
+            System.out.print("Nothing left to redo");
+        }
+    }
+
+    public void UndoAction() {
+
+        int test = CommandHistory.size() - undocount - 1;
+        if (CommandHistory.size() - undocount > 0) {
+            System.out.print("Command nr. " + test);
+            // Only undo if there's something to undo
+            CommandHistory.get(CommandHistory.size() - undocount - 1).Undo();
+            undocount++;
+        } else {
+            System.out.println("Undo failed, nothing left to undo");
+        }
+
+    }
+
+    private void ClearHistory() {
+        //Check if we can actually clear that amount of commands
+        if (undocount > 0) {
+            for (int i = 0; i < undocount; i++) {
+                this.CommandHistory.remove(CommandHistory.size() - 1);
+            }
+            undocount = 0;
+        }
+
+    }
+
+}
